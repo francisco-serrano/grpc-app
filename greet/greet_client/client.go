627a3d422eb99f213db3b561c0b8440a51258903
francisco-serrano <francisco.serrano372@gmail.com>
2020-02-23 20:06:38 -0300

    adds client request

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/francisco-serrano/grpc-app/greet/greetpb"
 	"google.golang.org/grpc"
 	"log"
 )
+
+func doUnary(c greetpb.GreetServiceClient) {
+	log.Printf("Starting to do a Unary RPC...")
+
+	request := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Francisco",
+			LastName:  "Serrano",
+		},
+	}
+
+	response, err := c.Greet(context.Background(), request)
+	if err != nil {
+		log.Fatalf("error while calling Greet RPC: %v", err)
+	}
+
+	log.Printf("Response from Greet: %v", response.Result)
+}
 
 func main() {
 	fmt.Println("Hello I'm a client")
@@ -19,5 +38,5 @@ func main() {
 
 	c := greetpb.NewGreetServiceClient(conn)
 
-	fmt.Printf("Created client: %f", c)
+	doUnary(c)
 }
